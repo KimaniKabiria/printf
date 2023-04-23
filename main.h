2f0898ef270afa4934a31c509e9c03f6e246a951
Kimani Kabiria <kabiriakimani@gmail.com>
2023-04-23 13:06:58 +0300

    Added Binary Print and edited header, decimal and printf

--- main.h
+++ main.h
@@ -24,10 +24,20 @@ int _strlen(char *s);
 void _buff_reset(char *buffer);
 int _numlen(int num);
 char *itos(char str[], long int n);
+char *utos(char str[], unsigned int n);
 
 void print_char(va_list valist, char *buffer, int *index);
 void print_str(va_list list, char *buffer, int *index);
 void print_perc(va_list valist, char *buffer, int *index);
 void print_decimal(va_list list, char *buffer, int *indx);
+void print_udecimal(va_list list, char *buffer, int *indx);
+void print_reverse(va_list list, char *buffer, int *indx);
+void print_binary(va_list list, char *buffer, int *indx);
+void print_R(va_list list, char *buffer, int *indx);
+void print_hexa(va_list list, char *buffer, int *indx);
+void print_chexa(va_list list, char *buffer, int *indx);
+void print_octa(va_list list, char *buffer, int *indx);
+void print_pointer(va_list list, char *buffer, int *indx);
+void print_lx(va_list list, char *buffer, int *indx);
 
 #endif
--- print_decimal.c
+++ print_decimal.c
@@ -50,6 +50,38 @@ char *itos(char str[], long int n)
 }
 
 /**
+ * utos - uint to string
+ * @str: string
+ * @num: num
+ *
+ * Return: char array
+ */
+char *utos(char str[], unsigned int num)
+{
+	int i, rem, len = 0, n;
+
+	n = num;
+	while (n != 0)
+	{
+		len++;
+		n /= 10;
+	}
+	i = 0;
+
+	while (i < len)
+	{
+		rem = num % 10;
+		num = num / 10;
+
+		str[len - (i + 1)] = rem + '0';
+		i++;
+	}
+
+	str[len] = '\0';
+	return (str);
+}
+
+/**
  * print_decimal - printer for type d
  * @list: arguments passed
  * @buffer: values stored
@@ -82,4 +114,37 @@ void print_decimal(va_list list, char *buffer, int *indx)
 		buffer[*indx] = str[j];
 	}
 	free(str);
+}
+
+
+/**
+ * print_udecimal - printer for type u
+ * @list: list
+ * @buffer: buffer
+ * @indx: indx
+ * Return: void
+ */
+void print_udecimal(va_list list, char *buffer, int *indx)
+{
+	unsigned int i;
+	int j, numlen;
+	char *num_str;
+
+	i = va_arg(list, int);
+	numlen = _numlen(i);
+	num_str = malloc(numlen * sizeof(char));
+	if (num_str == NULL)
+		return;
+	utos(num_str, i);
+	for (i = *indx, j = 0; j < numlen; *indx += 1, i++, j++)
+	{
+		if (*indx == 1024)
+		{
+			_printer(buffer, *indx);
+			_buff_reset(buffer);
+			*indx = 0;
+		}
+		buffer[*indx] = num_str[j];
+	}
+	free(num_str);
 }
